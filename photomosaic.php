1327d389465398a1608c13fc623571acf89e0beb
Michael Kafka <michael.kafka@thomsonreuters.com>
2014-03-13 18:10:56 -0400

    2.6.3 - more filename capitalization

--- photomosaic.php
+++ photomosaic.php
@@ -5,7 +5,7 @@ Plugin URI: http://codecanyon.net/item/photomosaic-for-wordpress/243422?ref=makf
 Description: Adds a new display template for your WordPress and NextGen galleries. See the <a href="/wp-admin/admin.php?page=photomosaic">options page</a> for examples and instructions.
 Author: makfak
 Author URI: http://www.codecanyon.net/user/makfak?ref=makfak
-Version: 2.6.2
+Version: 2.6.3
 GitHub Plugin URI: daylifemike/photomosaic-for-wordpress
 */
 
@@ -22,7 +22,7 @@ class PhotoMosaic {
     public static $URL_PATTERN = "(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))";
 
     public static function version () {
-        return '2.6.2';
+        return '2.6.3';
     }
 
     public static function init() {
@@ -680,7 +680,7 @@ class PhotoMosaic {
 
     public static function render_admin_page() {
         if ( !class_exists('MarkdownExtra_Parser') ) {
-            require_once( 'includes/Markdown.php' );
+            require_once( 'includes/markdown.php' );
         }
         $options = PhotoMosaic::get_options();
         $options = PhotoMosaic::adjust_deprecated_options($options);
