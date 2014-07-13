878f8ba50eda1112394d768a3a906be37ebbc62a
Michael Kafka <michael.kafka@thomsonreuters.com>
2014-07-13 00:07:33 -0400

    2.8 - moves to React, moves to Layout.Columns, adds update and refresh interface

--- photomosaic.php
+++ photomosaic.php
@@ -5,7 +5,7 @@ Plugin URI: http://codecanyon.net/item/photomosaic-for-wordpress/243422?ref=makf
 Description: Adds a new display template for your WordPress and NextGen galleries. See the settings page for examples and instructions.
 Author: makfak
 Author URI: http://www.codecanyon.net/user/makfak?ref=makfak
-Version: 2.7.5
+Version: 2.8
 GitHub Plugin URI: daylifemike/photomosaic-for-wordpress
 */
 
@@ -22,7 +22,7 @@ class PhotoMosaic {
     public static $URL_PATTERN = "(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))";
 
     public static function version () {
-        return '2.7.5';
+        return '2.8';
     }
 
     public static function init() {
@@ -81,7 +81,7 @@ class PhotoMosaic {
             'prevent_crop' => false,
             'show_loading' => false,
             'loading_transition' => 'fade',
-            'responsive_transition' => true,
+            'resize_transition' => true,
             'lazyload' => 200,
             'lightbox' => true,
             'lightbox_rel' => 'pmlightbox',
@@ -144,6 +144,12 @@ class PhotoMosaic {
             unset($options['link_to_url']);
         }
 
+        // 'resposive_transition' renamed to 'resize_transitiion' - v2.8
+        if (array_key_exists('responsive_transition', $options)) {
+            $options['resize_transitiion'] = $options['responsive_transition'];
+            unset($options['responsive_transition']);
+        }
+
         update_option('photomosaic_options', $options);
 
         return $options;
@@ -182,7 +188,7 @@ class PhotoMosaic {
         );
         $bool_settings = array(
             'center', 'prevent_crop', 'links', 'external_links', 'show_loading',
-            'responsive_transition', 'lightbox', 'custom_lightbox', 'lightbox_group'
+            'resize_transition', 'lightbox', 'custom_lightbox', 'lightbox_group'
         );
         $int_false_settings = array('lazyload');
 
@@ -269,7 +275,7 @@ class PhotoMosaic {
                         external_links: '. $settings['external_links'] .',
                         show_loading: '. $settings['show_loading'] .',
                         loading_transition: "'. $settings['loading_transition'] .'",
-                        responsive_transition: '. $settings['responsive_transition'] .',
+                        resize_transition: '. $settings['resize_transition'] .',
                         lazyload: '. $settings['lazyload'] .',
                         modal_name: "' . $settings['lightbox_rel'] . '",
                         modal_group: ' . $settings['lightbox_group'] . ',
@@ -316,7 +322,7 @@ class PhotoMosaic {
                         var self = this;
 
                         jQuery("a", $photomosaic).each(function () {
-                            $a = $(this);
+                            $a = jQuery(this);
                             $img = $a.find("img");
                             id = $img.attr("id");
                             data = PhotoMosaic.Utils.deepSearch( self.images, "id", id );
